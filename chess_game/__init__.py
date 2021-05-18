14fbe5385c3d918a26ab374229a5d141b039a085
Billy Moore <202046@kedst.ac.uk>
2021-05-18 12:47:10 +0100

    Added more details to "chess_game" docstring

--- chess_game/__init__.py
+++ chess_game/__init__.py
@@ -1,4 +1,24 @@
 """chess_game module
 
 Implementation of chess rules.
+
+Classes:
+    GameState - The current position of the board & the moves already made & the pieces on the board
+    Piece (Abstract) - The abstract class for a piece
+        King
+        Queen
+        Rook
+        Knight
+        Bishop
+        Pawn
+    Move - Represents the movement from one square to another
 """
+
+class GameState:
+    """
+    The current state of the game including the Pieces and moves already made
+
+    Methods:
+        GameState(starting_position : string);
+
+    """
